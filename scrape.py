371ca3931da2e2131cb81de2166b99a92a26a710
hkon123 <hkonhd@gmail.com>
2019-06-02 22:48:54 +0200

    updated to run continously

--- scrape.py
+++ scrape.py
@@ -13,7 +13,7 @@ test3 = 'MerVerdi'
 
 sites = []
 links = []
-f=open('regnskapsføreroslo.txt', 'r')
+f=open(r'regnskapsføreroslo.txt', 'r')
 for line in f:
     list = line.split(' ')
     links.append(list[1])
@@ -25,39 +25,16 @@ page2 = links[1]
 
 #link = 'https://www.google.com/search?safe=off&ei=hEDwXKnnGNKvrgS03rmgBg&q=regnskapsf%C3%B8rer+oslo&oq=regnskapsf%C3%B8rer+oslo&gs_l=psy-ab.3..0i71l8.0.0..68731...0.0..0.0.0.......0......gws-wiz.F6biEh8buaw'
 #link2 = 'https://www.google.com/search?ei=eEDwXKPtMujmrgSG-ICoCg&q=regnskapsf%C3%B8rer+oslo&oq=regnskapsf%C3%B8rer+oslo&gs_l=psy-ab.3..0i71l8.0.0..123644...0.0..0.0.0.......0......gws-wiz.EZPrdvU61Yg'
-page = requests.get(page1)
-tree = html.fromstring(page.content)
+for i in range(10):
+    page = requests.get(page1)
+    tree = html.fromstring(page.content)
 
-sites.append(tree)#.xpath('//a[@title]/text()'))
+    sites.append(tree)#.xpath('//a[@title]/text()'))
 
-first = False
-text = page.text.split(' ')
-found = 0
-placementTest = 0
-placement = 0
-for word in text:
-    if 'href' in word:
-        if '/url' in word:
-            #print(word)
-            placementTest +=1
-            if test1 in word:
-                found += 1
-                first = True
-                placement = placementTest
-            elif test2 in word:
-                found += 1
-                first = True
-                placement = placementTest
-            elif test3 in word:
-                found += 1
-                first = True
-                placement = placementTest
-
-if found == 0:
-    #page2 = 'https://www.google.com/search?q=regnskapsf%C3%B8rer+oslo&ei=9UDwXNf4A4bMrgS_p6OgAw&start=10&sa=N&ved=0ahUKEwiXx63Zj8TiAhUGposKHb_TCDQQ8tMDCI8C&biw=1280&bih=610'
-    page = requests.get(page2)
+    first = False
     text = page.text.split(' ')
-    placementTest = 10
+    found = 0
+    placementTest = 0
     placement = 0
     for word in text:
         if 'href' in word:
@@ -66,41 +43,66 @@ if found == 0:
                 placementTest +=1
                 if test1 in word:
                     found += 1
+                    first = True
                     placement = placementTest
                 elif test2 in word:
                     found += 1
+                    first = True
                     placement = placementTest
                 elif test3 in word:
                     found += 1
+                    first = True
                     placement = placementTest
 
+    if found == 0:
+        #page2 = 'https://www.google.com/search?q=regnskapsf%C3%B8rer+oslo&ei=9UDwXNf4A4bMrgS_p6OgAw&start=10&sa=N&ved=0ahUKEwiXx63Zj8TiAhUGposKHb_TCDQQ8tMDCI8C&biw=1280&bih=610'
+        page = requests.get(page2)
+        text = page.text.split(' ')
+        placementTest = 10
+        placement = 0
+        for word in text:
+            if 'href' in word:
+                if '/url' in word:
+                    #print(word)
+                    placementTest +=1
+                    if test1 in word:
+                        found += 1
+                        placement = placementTest
+                    elif test2 in word:
+                        found += 1
+                        placement = placementTest
+                    elif test3 in word:
+                        found += 1
+                        placement = placementTest
 
 
-named_tuple = time.localtime() # get struct_time
-time_string = time.strftime("%m.%d.%Y, %H:%M:%S", named_tuple)
 
-wb = load_workbook('google.xlsx')
-ws = wb.active
-id = ws.cell(row=2, column=1)
-current_row = 2
-while id.value!= None:
-    current_row +=1
-    id = ws.cell(row = current_row, column = 1)
+    named_tuple = time.localtime() # get struct_time
+    time_string = time.strftime("%m.%d.%Y, %H:%M:%S", named_tuple)
 
-if found != 0 and first == True:
-    ws.cell(row = current_row, column = 1, value = current_row-1)
-    ws.cell(row = current_row, column = 2, value = time_string)
-    ws.cell(row = current_row, column = 3, value = placement)
-    ws.cell(row = current_row, column = 4, value = 'True')
-    ws.cell(row = current_row, column = 5, value = 'MerVerdi')
-    ws.cell(row = current_row, column = 6, value = 'regnskapsfører oslo')
+    wb = load_workbook('google.xlsx')
+    ws = wb.active
+    id = ws.cell(row=2, column=1)
+    current_row = 2
+    while id.value!= None:
+        current_row +=1
+        id = ws.cell(row = current_row, column = 1)
 
-elif found != 0 and first == False:
-    ws.cell(row = current_row, column = 1, value = current_row-1)
-    ws.cell(row = current_row, column = 2, value = time_string)
-    ws.cell(row = current_row, column = 3, value = placement)
-    ws.cell(row = current_row, column = 4, value = 'False')
-    ws.cell(row = current_row, column = 5, value = 'MerVerdi')
-    ws.cell(row = current_row, column = 6, value = 'regnskapsfører oslo')
+    if found != 0 and first == True:
+        ws.cell(row = current_row, column = 1, value = current_row-1)
+        ws.cell(row = current_row, column = 2, value = time_string)
+        ws.cell(row = current_row, column = 3, value = placement)
+        ws.cell(row = current_row, column = 4, value = 'True')
+        ws.cell(row = current_row, column = 5, value = 'MerVerdi')
+        ws.cell(row = current_row, column = 6, value = 'regnskapsfører oslo')
 
-wb.save('google.xlsx')
+    elif found != 0 and first == False:
+        ws.cell(row = current_row, column = 1, value = current_row-1)
+        ws.cell(row = current_row, column = 2, value = time_string)
+        ws.cell(row = current_row, column = 3, value = placement)
+        ws.cell(row = current_row, column = 4, value = 'False')
+        ws.cell(row = current_row, column = 5, value = 'MerVerdi')
+        ws.cell(row = current_row, column = 6, value = 'regnskapsfører oslo')
+
+    wb.save('google.xlsx')
+    time.sleep(7200)
